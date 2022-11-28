8548b2b2c49762262e32c2424fa3d4956be5e599
mogera551 <mogera551@gmail.com>
2022-11-29 08:17:01 +0900

    [rename]re-data-x.js to re-data-x.min.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry : './src/index.js',
   output : {
-    filename    : 're-data-x.js',
+    filename    : 're-data-x.min.js',
     path        : path.resolve(__dirname, 'dist')
   },
   mode : "production",
