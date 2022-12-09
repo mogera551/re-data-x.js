bce845b0d0d058ea178dd7517030b3df69a8ed54
mogera551 <mogera551@gmail.com>
2022-12-10 07:45:40 +0900

    [add] webpack config for develop

---
+++ webpack.config.dev.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  entry : './src/index.js',
+  output : {
+    filename    : 're-data-x.js',
+    path        : path.resolve(__dirname, 'dist')
+  },
+  mode : "development",
+};
